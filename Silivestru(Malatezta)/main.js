06f39f3db765a8bc1bb43146c7f6847115cf9f9e
Silivestru <sgoncearenco92@gmail.com>
2021-01-27 15:04:56 +0000

    #7#2 If statement

---
+++ Silivestru(Malatezta)/main.js
@@ -0,0 +1,19 @@
+// 1. Printing something to the console//
+
+console.log("I am lost!")
+
+//2. Showing an if statement//
+
+let favMusic = "electronic"
+
+if (music == "classical"){
+    console.log ("Mmmm.. classical music. Still good!")
+}
+
+else if (music == "pop"){
+    console.log("Please change the track asap")
+}
+
+else {
+    console.log("Oh nice... nice trying new music")
+}
