bf9b1d7ca2aca03673e735299cfe75bf3283c19f
Sarah Wright <s.wright7@outlook.com>
2021-01-27 19:48:03 +0000

    #7 sarahnwright
    adding if statement

--- sarahnwright/example.js
+++ sarahnwright/example.js
@@ -1,2 +1,12 @@
 console.log("Good Morning, Good Afternoon, Good Evening, and Goodnight");
 
+let weather = "sunny";
+    if (weather == "sunny"){
+        console.log ("What a lovely day to get outside for a walk!");
+    }
+    else if (weather == "raining"){
+        console.log ("Get your umbrella and wellies, and get out for a walk!");
+    }
+    else {
+        console.log ("What a bland day, sit back for a Netflix binge!")
+    }
