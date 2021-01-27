1aa36aab2105c530965963f88f2d56c01b615599
Silivestru <sgoncearenco92@gmail.com>
2021-01-27 15:39:20 +0000

    #7#3 Switch case

--- Silivestru(Malatezta)/main.js
+++ Silivestru(Malatezta)/main.js
@@ -17,3 +17,27 @@ else if (music == "pop"){
 else {
     console.log("Oh nice... nice trying new music")
 }
+
+// 3. Showing a switch case//
+
+let favDish = "Lasagna";
+switch(favDish){
+    case "Tortilla":
+    case "Gazpacho":
+    console.log("Mmmm, tasty... you must love Spanish gastronomy");
+    break;
+    case "Fish&Chips":
+    case "Sunday Roast":
+    console.log("I be you travelled to UK before");
+    break;
+    case "Lasagna":
+    case "Spaghetti carbonara":
+    case "Risotto":
+    console.log("That's my favorite country's food: ITALY!");
+    break;
+    case "Mamaliga":
+    console.log("That sounds interesting. Is this a Romanian dish?");
+    break;
+    default:
+    console.log("You are to picky when choosing what to eat. Sorry!");
+}
