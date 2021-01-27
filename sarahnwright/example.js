5d91abda3f8c88b42df5dcbd434483e7e3bdf925
Sarah Wright <s.wright7@outlook.com>
2021-01-27 20:03:10 +0000

    #7 sarahnwright
    adding switch case

--- sarahnwright/example.js
+++ sarahnwright/example.js
@@ -10,3 +10,24 @@ let weather = "sunny";
     else {
         console.log ("What a bland day, sit back for a Netflix binge!")
     }
+
+//3.showing a switch case
+
+console.log ("Where do you like to holiday?");
+let holiday = "Nice";
+    switch (holiday){
+            case "Albufera":
+            case "Barcelona":
+            case "Cadiz":
+                console.log ("You like to holiday in Spain!");
+                break;
+            case "Paris":
+            case "Nice":
+                console.log ("You like to holiday in France! Oo lala");
+                break;
+            case "Venice":
+            case "Sardinia":
+                console.log ("You like to holiday in Italy!")
+            default:
+                console.log ("You would rather stay at home than go on holiday?")
+    }
