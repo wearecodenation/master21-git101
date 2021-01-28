9fbf1a0d238b7c13dd0a4e2712787da8079883a3
Joe Foster <jfoster29492@gmail.com>
2021-01-28 12:27:03 +0000

    #7 1. Created JS program
    Fixed index.html, added JS program with example of printing to console

---
+++ JoeFoster-cn/node.js
@@ -0,0 +1,16 @@
+/*In your user area write a small nodejs program, this should demonstrate your understanding of the core principles we studied in week 1. 
+It should include an example of:
+1. Printing something to the console
+2. Showing an if statement
+3. Showing a switch case
+4. Show an example of an array
+5. Show an example of a for loop
+6. Show an example of a do loop
+7. Show an example of a function
+You should not do this all at once, you should do each of these one by one, commit and push them, to practice git.
+You should also include this ticket number in your git commits so we can see how they are all linked.*/
+
+let myage = 28;
+let myname = "Joe"; {
+
+console.log(`My name is ${myname} and I am ${myage} years old.`) }
