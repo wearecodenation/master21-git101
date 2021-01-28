7687e7f4cd18aae51be704fd3638922ef1609b55
Gemcila <ggemcila@gmail.com>
2021-01-28 13:05:25 -0800

    #7 2) Added an if statement to the node.js file

--- ggemcila/node.js
+++ ggemcila/node.js
@@ -1,2 +1,9 @@
 /* 1) Printing something on the console*/
 console.log("Welcome to Jevascript!");
+/* 2) Showing an if statement*/
+let javascriptKnowledge = 70;
+if (javascriptKnowledge < 60) {
+    console.log("You need to revise again!");
+} else{
+    console.log("You are on the right track, keep it up!");
+}
