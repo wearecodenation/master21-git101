4d56ab34fce64192b436b4ecd53784b2f6243231
Adam Manley <adam.p.manley@gmail.com>
2021-01-28 14:17:13 +0000

    #7 Added if statement

--- ap42m/git.js
+++ ap42m/git.js
@@ -1,1 +1,31 @@
+// In your user area write a small nodejs program, this should demonstrate your understanding of the core principles we studied in week 1. It should include an example of:
+
+
+// if statement
+// switch statyement 
+// Show an example of an array
+// Show an example of a for loop
+// Show an example of a do loop
+// Show an example of a function
+// You should not do this all at once, you should do each of these one by one, commit and push them, to practice git.
+
+
+// console.log
 console.log("Why are you here?")
+
+//if satement
+var x
+var y
+x = 166
+y = 15
+console.log(x)
+if (x==y){                                  // needed strict equals for type and value
+    console.log("your numbers are equal")
+}
+else if (x>y){ 
+    
+    console.log("x is greater than y")
+}
+else{
+    console.log("y is greater than x")
+}
