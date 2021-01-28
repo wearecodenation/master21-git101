a55c7fda2f3005bd42f4dd787a21a7530739fdc4
Jake Dibbert <jake.dibbert@sofology.co.uk>
2021-01-28 11:28:28 +0000

    #7 Created an if statement

--- JakeyD11/node.js
+++ JakeyD11/node.js
@@ -1,4 +1,16 @@
-let message = "";
-
+let software = "Git";
+let task = " ";
+let mood =" ";
+let message = " ";
 message = "Hello there";
 console.log(message);
+
+software = "VS code";
+mood = "Good";
+
+if (software == "Git" && mood =="Good") {
+    console.log("You can do this!");
+}
+else if (software != "Git") {
+    console.log("finally a break from Git");
+}
