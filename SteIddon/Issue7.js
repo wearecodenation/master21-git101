5566fdcda4caf19fbdb6ec167c9df50c6c08a266
Ste Iddon <steiddon@gmail.com>
2021-01-28 17:17:17 +0000

    #7 .2 - If statement

--- SteIddon/Issue7.js
+++ SteIddon/Issue7.js
@@ -1,3 +1,11 @@
 console.log("Hello World!");
 
 
+var password = "giraffe"
+
+if (password.length <= 8) {
+    console.log("Sorry, this password is too short");
+   }   else {
+        console.log("Thank you for entering your password");
+    }
+    
