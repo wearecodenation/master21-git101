e529947f5f50a607f6646fe33b6213dff7557cb9
Jake Dibbert <jake.dibbert@sofology.co.uk>
2021-01-28 11:35:40 +0000

    #7 Showing a switch case
    added a switch case what we are doing

--- JakeyD11/node.js
+++ JakeyD11/node.js
@@ -1,16 +1,32 @@
-let software = "Git";
+let program = "Git";
 let task = " ";
 let mood =" ";
 let message = " ";
+
 message = "Hello there";
 console.log(message);
 
-software = "VS code";
+program = "VS code";
 mood = "Good";
 
-if (software == "Git" && mood =="Good") {
+if (program == "Git" && mood =="Good") {
     console.log("You can do this!");
 }
-else if (software != "Git") {
+else if (program != "Git") {
     console.log("finally a break from Git");
+}
+
+switch(program){
+    case "VS code":
+    case "HTML":
+    case "Javascript":
+            console.log("Alright lets do this!");
+            break;
+    case "Github":
+    case "GitKraken":
+    case "Git":
+            console.log("Fucking Git again eugh");
+            break;
+        default:
+            console.log("Must be the weekend if you are doing nothing!")
 }
