b5d143d0a75670e8b8747efb71c884e26ad791e6
Evan Morris <evanmorris97@gmail.com>
2021-01-28 12:23:59 +0000

    #7 Added an if statement to the js file

--- evanmorrisdev/evanmorrisdev.js
+++ evanmorrisdev/evanmorrisdev.js
@@ -1,6 +1,6 @@
 // In your user area write a small nodejs program, this should demonstrate your understanding of the core principles we studied in week 1. It should include an example of:
 
-// Showing an if statement
+
 // Showing a switch case
 // Show an example of an array
 // Show an example of a for loop
@@ -9,4 +9,16 @@
 // You should not do this all at once, you should do each of these one by one, commit and push them, to practice git.
 
 
-console.log("Hello World!"); //1. Print something to the console.
+//1. Print something to the console.
+console.log("Hello World!");
+
+//2. Showing an if statement
+var firstNumber = 3;
+var secondNumber = 0.5;
+
+if ((firstNumber * secondNumber) > (firstNumber + secondNumber)) {
+    console.log(firstNumber + " x " + secondNumber + " is greater than " + firstNumber + " + " + secondNumber);
+}
+else {
+    console.log(firstNumber + " x " + secondNumber + " is greater than " + firstNumber + " + " + secondNumber);
+}
