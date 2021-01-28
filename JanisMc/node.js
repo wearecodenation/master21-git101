5f20395a3cb6a1066dddbb9cd5d7d361f94ce2f6
Janis <janismcluckie@googlemail.com>
2021-01-28 17:04:41 +0000

    #7 Added do while loop Amended first console.log

--- JanisMc/node.js
+++ JanisMc/node.js
@@ -1,26 +1,26 @@
-console.log("Please help, I think I'm losing my mind");
+// let brain = "Fried";
 
-let brain = "Fried"
+// console.log(`Please help, I think I'm losing my mind it's ${brain}`);
 
-if (brain == "Fried") {
-    console.log("Please help me")
-}
-else {
-    console.log("You must be doing ok")
-}
+// if (brain == "Fried") {
+//     console.log("Please help me")
+// }
+// else {
+//     console.log("You must be doing ok")
+// }
 
-    switch (brain) {
-        case "Fried":
-        case "Still fried":
-        console.log("I think I'm losing my mind");
-        break;
-        case "Ok":
-        case "Great":
-        console.log("Seems like you're doing better");
-        break;
-        default:
-        console.log("How are you?")
-    }
+//     switch (brain) {
+//         case "Fried":
+//         case "Still fried":
+//         console.log("I think I'm losing my mind");
+//         break;
+//         case "Ok":
+//         case "Great":
+//         console.log("Seems like you're doing better");
+//         break;
+//         default:
+//         console.log("How are you?")
+//     }
 
 let array = [
     "Robert Plant",
@@ -33,3 +33,7 @@ for(let ledZepInd = 0; ledZepInd < array.length; ledZepInd++){
     console.log(array[ledZepInd])
 }
 
+do { 
+    array.pop()
+}
+while (array.length < 4);
