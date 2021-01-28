204e4e1e9ed15e87a24ecff40aad5b00c3523280
Jim <jadavey@hotmail.com>
2021-01-28 11:58:36 +0000

    #7.3
    Task 7 part 3 complete

--- jadavey91/task7.js
+++ jadavey91/task7.js
@@ -1,3 +1,21 @@
+id = "Niel";
+
+console.log(id);
+
+if (id =="Jim Davey") {
+    
+    console.log("You are the best")
+}
+else {
+    console.log(`Hold up... ${id} what are you doing here? You're not Jim, get outta here!!`)
+}
+
 id = "Jim Davey"
 
-console.log(id)
+switch (id){
+    case "Jim Davey":
+        console.log(`Phew ${id}, you're here!`);
+        break;
+    default:
+        console.log("Where is Jim? Please can you go get him to sort this mess out.");
+}
