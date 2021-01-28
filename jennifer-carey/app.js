075a9f6b9467c0a27be606775fa4bd8bc9250743
Jennifer <jennifacarey@gmail.com>
2021-01-28 13:13:58 +0000

    #7 2. Added an if statement

--- jennifer-carey/app.js
+++ jennifer-carey/app.js
@@ -2,3 +2,13 @@
 // Printing something to the console
 
 console.log("Hello, my name is Jennifer.");
+
+// Showing an if statement
+
+const age = 38;
+
+if (age > 30) {
+   console.log(`I'm ${age} years old so I am getting on a bit.`)
+} else {
+   console.log(`I'm ${age} years old - a spring chicken!`);
+}
