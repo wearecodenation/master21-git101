385a072a2451f777fb53147472802e24ced9728b
Ruaraidh <rca.plummer@gmail.com>
2021-01-28 14:43:27 +0000

    #7.2 +7.3

--- ruarplum/node.js
+++ ruarplum/node.js
@@ -6,12 +6,35 @@ console.log(x)
 // node node.js - prints to terminal
 
 
-
 // 2. Showing an if statement
 
+let password = "stephanie"
+
+if (password.length < 8){
+    console.log("password must be 8 characters or more");
+}
+else{
+    console.log("Your new password has been accepted");
+}
 
 
 // 3. Showing a switch case
+
+let topping = "pinapple"
+
+switch (topping){
+    case "Salami":
+    case "cheese":
+    case "chilli":
+    case "olives":
+    case "anchovies":
+        console.log("This is a key pizza topping")
+        break;
+    case "pinapple":
+    case "courgettes":
+        console.log("what is this doing on a pizza!");
+}
+
 
 // 4. Show an example of an array
 
