d72e532c55fbd42f89f726cd67c94580b3c70a82
Amelia <amelia.riding@Sofology.co.uk>
2021-01-28 14:19:37 +0000

    #7 Ifelse Added

--- Melia-create/node.js
+++ Melia-create/node.js
@@ -1,1 +1,12 @@
+//1
 console.log ("Hello World!");
+
+//2
+let password = "WooWoo"
+
+if (password.length <=8) {
+    console.log("your Password is too short")
+}
+else if (password.length >=8) {
+    console.log("Your Password is set")
+}
