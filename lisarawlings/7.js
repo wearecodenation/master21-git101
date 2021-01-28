4329a348daa3869c6bdb1d3209307dd31b120e33
Lisa Rawlings <lisa-rawlings@hotmail.com>
2021-01-28 12:36:57 +0000

    #7 Showing an if statement

--- lisarawlings/7.js
+++ lisarawlings/7.js
@@ -1,2 +1,12 @@
 //Print something to the console//
 console.log (Math.random()*10);
+
+//`Showing` an "if" statement//
+let PIN=4322
+
+if (PIN===4321){
+    console.log("How much would you like to withdraw?")
+}
+else {
+    console.log("Sorry! Wrong PIN, try again.")
+}
