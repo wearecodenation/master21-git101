52a58b4b817333bc21b97219399a82330494bea5
Philip <philipedwards81@live.co.uk>
2021-01-28 15:10:39 +0000

    #7
    7.3 switch case

--- phiddle/seven.js
+++ phiddle/seven.js
@@ -5,3 +5,16 @@ let favefilm = (`Star Wars`);
 if (favefilm <= `star wars`);
 console.log("SORRY WE DONT HAVE THAT");
 
+function movieLine(tagLine) {
+    switch (tagLine) {
+    case `Arnold`:
+        return `i'll be back`;
+    case `Bruce`:
+        return `ippy kai aye`;
+    case `Sylvester`:
+        return `Dont throw momma from the train`;
+    default: 
+        return `Your gonna need a bigger boat`;
+    }
+}
+console.log(movieLine(`arnold`));
