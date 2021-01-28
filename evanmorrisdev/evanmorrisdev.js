1980bf816cb1d4f162661e5543470d6bae57829a
Evan Morris <evanmorris97@gmail.com>
2021-01-28 12:08:22 +0000

    #7 new file evanmorrisdev.js with Hello World printed to the console.

---
+++ evanmorrisdev/evanmorrisdev.js
@@ -0,0 +1,12 @@
+// In your user area write a small nodejs program, this should demonstrate your understanding of the core principles we studied in week 1. It should include an example of:
+
+// Showing an if statement
+// Showing a switch case
+// Show an example of an array
+// Show an example of a for loop
+// Show an example of a do loop
+// Show an example of a function
+// You should not do this all at once, you should do each of these one by one, commit and push them, to practice git.
+
+
+console.log("Hello World!"); //1. Print something to the console.
