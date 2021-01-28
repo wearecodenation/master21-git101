cd2dfb59c49d986c08423bca3ff40e6816096613
Janis <janismcluckie@googlemail.com>
2021-01-28 16:33:28 +0000

    #7 Added if statement

--- JanisMc/node.js
+++ JanisMc/node.js
@@ -1,1 +1,10 @@
 console.log("Please help, I think I'm losing my mind");
+
+let brain = "Fried"
+
+if (brain == "Fried") {
+    console.log("Please help me")
+}
+else {
+    console.log("You must be doing ok")
+}
