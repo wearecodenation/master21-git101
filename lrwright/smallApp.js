6ddb1fec7c055ab22756c676343f1942933b39d2
Le-Ann <leannwright21@gmail.com>
2021-01-28 14:54:11 +0000

    #7 task 2 added switch case

--- lrwright/smallApp.js
+++ lrwright/smallApp.js
@@ -1,10 +1,16 @@
+
 // Printing something to the console
 console.log("Welcome to the app");
 
-
-
-
+let age = 10;
 // Showing an if statement
+if (age<18) {
+    console.log("sorry can't serve you")
+  } else {
+    console.log("Have a pint")
+  }
+console.log();
+
 // Showing a switch case
 // Show an example of an array
 // Show an example of a for loop
