5b28774e5d4bc2fae33f6e0fb65ee063ca5fdfc1
Le-Ann <leannwright21@gmail.com>
2021-01-28 14:44:45 +0000

    #7 task 1 print to console

---
+++ lrwright/smallApp.js
@@ -0,0 +1,13 @@
+// Printing something to the console
+console.log("Welcome to the app");
+
+
+
+
+// Showing an if statement
+// Showing a switch case
+// Show an example of an array
+// Show an example of a for loop
+// Show an example of a do loop
+// Show an example of a function
+
