687147990ffcda67ce3cb09174dce8b200dc24bd
Jennifer <jennifacarey@gmail.com>
2021-01-28 14:05:26 +0000

    #7 3.  Added a switch case example

--- jennifer-carey/node.js
+++ jennifer-carey/node.js
@@ -15,3 +15,30 @@ if (age > 30) {
 
 // Showing a switch case
 
+const day = new Date().getDay();
+
+switch (day) {
+   case 1:
+      console.log("Welcome to the start of the week!");
+      break;
+   case 2:
+      console.log("Happy Tuesday!");
+      break;
+   case 3:
+      console.log("Happy hump day!");
+      break;
+   case 4:
+      console.log("It's almost the weekend!");
+      break;
+   case 5:
+      console.log("TFI Friday!");
+      break;
+   case 6:
+      console.log("Enjoy your Saturday!");
+      break;
+   case 7:
+      console.log("Take it easy, it's Sunday.");
+      break;
+   default:
+      console.log("Error: what happened to the days?");
+}
