7f0a1794358ebff12a749e3fb755d926d8cb6f77
Riley <rileyxcroft@gmail.com>
2021-01-28 11:39:58 +0000

    #7 If statement

--- Riley142/script.js
+++ Riley142/script.js
@@ -1,2 +1,11 @@
 // Printing something to the console
 console.log("Something.");
+
+// Showing an if statement
+let sleepy = true;
+
+if  (sleepy = false) {
+    console.log("No need for caffeine :D!");
+} else (sleepy = true); {
+    console.log("Need to drink some coffee!");
+}
