3f1a35a6a4661f999ec63ea14275a1409e932ec2
Nicholas Ferrari <Nick.ferrari@me.com>
2021-01-28 14:05:17 +0000

    #7 part 2 showing if statement

--- NickFerra/issue7.js
+++ NickFerra/issue7.js
@@ -1,3 +1,12 @@
 //printing to console
-
 console.log("Hello world");
+
+//Showing an if statement
+let eggs = 4;
+
+if(eggs >= 6){
+    console.log ("The chickens are doing well");
+}
+else {
+    console.log("we need more chickens");
+}
