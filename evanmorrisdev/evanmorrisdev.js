37ff0384f75c93ac7a1d3506855c85366d400794
Evan Morris <evanmorris97@gmail.com>
2021-01-28 13:05:55 +0000

    #7 Added a for loop

--- evanmorrisdev/evanmorrisdev.js
+++ evanmorrisdev/evanmorrisdev.js
@@ -1,9 +1,4 @@
-// In your user area write a small nodejs program, this should demonstrate your understanding of the core principles we studied in week 1. It should include an example of:
 
-
-
-// Show an example of an array
-// Show an example of a for loop
 // Show an example of a do loop
 // Show an example of a function
 // You should not do this all at once, you should do each of these one by one, commit and push them, to practice git.
@@ -40,3 +35,8 @@ switch (firstName) {
 
 //4. Show an example of an array
 let rockBands = new Array("Foo Fighters", "Royal Blood", "Arctic Monkeys"); 
+
+//5. Show an example of a for loop
+for (a = 0; a < rockBands.length; a++) {
+    console.log(rockBands[a])
+}
