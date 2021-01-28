d6c125e1d915d53a08ed939eba7ad5292cbaf9bc
Gemcila <ggemcila@gmail.com>
2021-01-28 13:07:13 -0800

    #7 3) Added a switch case

--- ggemcila/node.js
+++ ggemcila/node.js
@@ -7,3 +7,19 @@ if (javascriptKnowledge < 60) {
 } else{
     console.log("You are on the right track, keep it up!");
 }
+/* 3) Showing a switch case*/
+let git1 = "kind to me";
+let git2 = "conflicts";
+let git3 = "changes approved by reviewers";
+
+switch (git1) {
+    case "kind to me":
+        console.log(`I'm very happy that git is ${git1} today :)`);
+        break;
+    case "conflicts":
+        console.log(`Need to solve those ${git2} ASAP!!!`);
+        break;
+    case "changes approved by reviewers":
+        console.log("I'm ready to merge!");
+        break;
+}
