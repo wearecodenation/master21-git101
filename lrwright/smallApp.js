e3e98f0201d184475771ea8eb65eb428390e7fc0
Le-Ann <leannwright21@gmail.com>
2021-01-28 15:14:10 +0000

    #7 Task 5 For loop added

--- lrwright/smallApp.js
+++ lrwright/smallApp.js
@@ -3,29 +3,32 @@ console.log("Welcome to the app");
 
 let age = 10;
 // Showing an if statement
-if (age<18) {
+if (age < 18) {
     console.log("sorry can't serve you")
-  } else {
+} else {
     console.log("Have a pint")
-  }
+}
 
 // Showing a switch case
 
 let meal = "hotdogs"
 
-switch(meal) {
+switch (meal) {
     case "pizza": console.log("Oooh my favourite");
-    break;
+        break;
     default:
         console.log("It'll do...")
 }
 
 // Show an example of an array
-let faveGames = ['Ocarina of Time','Breath of the Wild', 'Majoras Mask','Stardew Valley'];
+let faveGames = ['Ocarina of Time', 'Breath of the Wild', 'Majoras Mask', 'Stardew Valley'];
 
 console.log(faveGames)
 
 // Show an example of a for loop
+for (let i= 0; i<10; i++) {
+    console.log(i);
+}
 // Show an example of a do loop
 // Show an example of a function
 
