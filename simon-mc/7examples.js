915daf8756b447586ede5808dd837db49a1fa46e
simon <simcnee@yahoo.co.uk>
2021-01-29 17:13:38 +0000

    #switch

--- simon-mc/7examples.js
+++ simon-mc/7examples.js
@@ -11,3 +11,23 @@ else {
     console.log ("lower than 10")
 }
 
+//Showing a switch case
+
+switch(drinks){
+
+    case"water":
+    console.log("boring");
+    break
+
+    case"orange juice":
+    console.log("nice");
+    break
+
+    case"beer":
+    console.log("even better");
+
+    default:
+        console.log("not a drink");
+}
+
+
