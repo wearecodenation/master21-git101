8c6c547c75ffb374cf008d7381bf35eb9b796d9f
Ste Iddon <steiddon@gmail.com>
2021-01-29 09:46:28 +0000

    #7 .3 - switch case

--- SteIddon/Issue7.js
+++ SteIddon/Issue7.js
@@ -1,6 +1,5 @@
 console.log("Hello World!");
 
-
 var password = "giraffe"
 
 if (password.length <= 8) {
@@ -8,4 +7,16 @@ if (password.length <= 8) {
    }   else {
         console.log("Thank you for entering your password");
     }
-    
+
+const expr = 'Pistachios';
+    switch (expr) {
+      case 'Hazelnuts':
+        console.log('Hazelnuts are £10 a kilo.');
+        break;
+      case 'Almonds':
+      case 'Macadamias':
+        console.log('Almonds and Macadamias are £12 a kilo.');
+        break;
+      default:
+        console.log(`Sorry, we are out of ${expr}.`);
+    }
