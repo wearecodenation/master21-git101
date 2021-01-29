121e988ca9889964b450a8ad59478645cc70b9c8
netty <netty.carroll@live.com>
2021-01-29 15:21:37 +0000

    #7 loop

--- netty-carroll/node.js
+++ netty-carroll/node.js
@@ -1,8 +1,7 @@
-let hour = 22;
-
-if (hour <18){
-console.log("buenos dias");
+let multiplesFive = [];
+for(let i = 0; i < 100; i++){
+    if (i % 5 == 0){
+        multiplesFive.push(i);
+    }
 }
-else {
-console.log("buenas noches");
-}
+console.log('numbers between 0 and 100 that are divisible by 5 are: ${multiplesFive}.');
