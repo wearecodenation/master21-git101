0bc6dc7b09a918368c41c93652b2eb239581d245
Ste Iddon <steiddon@gmail.com>
2021-01-29 10:56:33 +0000

    #8 Prompt & Alert

---
+++ SteIddon/Promptalert.js
@@ -0,0 +1,13 @@
+let userName = prompt("Please enter your name: ");
+
+alert(`Hello, ${userName} hope you are having a good day`);
+
+let eggsSold = prompt("How many eggs have we sold today? ");
+
+alert(`WOW! ${eggsSold} already?! LETS CELEBRATE!\nThe champagne is in the fridge`);
+
+let yesNo = prompt("Will Liverpool win the league?");
+
+alert(`${yesNo}?! Of course they will! Don't ask such silly questions`);
+
+
