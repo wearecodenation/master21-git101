0e206e58d05c74cebcc914b3fe7c31c313ac7dd3
Simon <77974137+simcnee@users.noreply.github.com>
2021-01-29 16:43:15 +0000

    message

---
+++ simon-mc/7examples.js
@@ -0,0 +1,13 @@
+// printing to the console
+console.log ("Hi There!")
+
+//Showing an if statement
+let num= "10";
+
+if (num >10) {
+    console.log("higher than 10")
+}
+else {
+    console.log ("lower than 10")
+}
+
