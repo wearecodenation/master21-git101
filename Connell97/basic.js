9179417a15e86c970730df95fa5bbaf1c6bb825a
Matt <m.connell97@hotmail.co.uk>
2021-01-29 16:21:02 +0000

    7.2 Added an example of an if statement

--- Connell97/basic.js
+++ Connell97/basic.js
@@ -1,1 +1,20 @@
 console.log("7.1 Complete!");
+
+let num = (Math.floor(Math.random()*10));
+
+function checkNum() {
+    if (num < 5) {
+        console.log(`Your number is lower than 5. Better luck next time!`);
+    }
+
+    if (num == 5) {
+        console.log(`You win! Congrats!`);
+    }
+
+    if (num > 5) {
+        console.log(`Your number is higher than 5. Better luck next time!`);
+    }
+}
+
+console.log(`Your number is ${num}`);
+checkNum();
