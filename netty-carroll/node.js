694fd37a2f707a02649c3c2ebe09643e06e078e6
netty <netty.carroll@live.com>
2021-01-29 11:32:01 +0000

    #7 switch case

--- netty-carroll/node.js
+++ netty-carroll/node.js
@@ -1,8 +1,13 @@
-let hour = 22;
-
-if (hour <18){
-console.log("buenos dias");
-}
-else {
-console.log("buenas noches");
-}
+let top = "onions"
+switch(top){
+    case "mushrooms":
+        case "jalapenos":
+            console.log("these are important for my pizza");
+            break;
+            case "peppers":
+                case "onions":
+                    console.log(`i dont mind ${top} on my pizza`);
+                    break;
+                    case "sausage":
+                        case "pepperoni":
+                            console.log(`${top} does not belong on my pizza`);}
